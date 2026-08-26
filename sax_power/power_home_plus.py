c98c10a55b0c6ae95fd80978ff8a040bfff70dfa
Hannes <hannes.kontos@gmx.de>
2026-08-26 19:02:58 +0200

    Add sax power home plus script (#1)

---
+++ sax_power/power_home_plus.py
@@ -0,0 +1,47 @@
+from pymodbus.client.sync import ModbusTcpClient
+
+class PowerHomePlus:
+    def __init__(
+        self,
+        ip: str,
+        port: int =502,
+        timeout: int =10
+    ):
+        self.client = ModbusTcpClient(
+            host=ip,
+            port=port,
+            timeout=timeout
+        )
+
+    @staticmethod
+    def apply_offset(value: int):
+        offset = 16384
+        return value - offset
+
+    def read_register(self):
+        """
+        46: battery charge level
+        47: battery power
+        48: battery grid power
+        """
+        addresses = [46, 47, 48]
+
+        results = []
+
+        for i in range(len(addresses)):
+            value = self.client.read_holding_registers(address=addresses[i], count=1, unit=64)
+
+            if not value.isError():
+                if i > 0:
+                    decoded_value = self.apply_offset(value.registers[0])
+                    results.append(decoded_value)
+                else:
+                    results.append(value.registers[0])
+            else:
+                print(f"Error reading register {addresses[i]} from Sax Power Home Plus Battery")
+                results.append(None)
+
+        return results
+
+    def close(self):
+        self.client.close()
